6dadc627c06aa80c62d10483e0210b8f4a50deaf
salmanra <rahmysalman@gmail.com>
2024-09-14 21:51:59 -0400

    no good, start from scratch next

--- lu.py
+++ lu.py
@@ -6,15 +6,13 @@ import scipy.linalg as la
 
 def gemm(A: np.ndarray) -> np.ndarray:
     n = A.shape[0] # A is a square matrix
-    P = np.zeros(n, dtype=np.int8)
+    P = np.arange(n, dtype=np.int8)
     for j in range (n):
         # find the max elt in this column
-        max_idx = np.argmax(A[:, j].__abs__(), axis=0) 
-        P[j] = max_idx
-        P[max_idx] = j
-        temp = A[max_idx, :]
-        A[max_idx, :] = A[j, :]
-        A[j, :] = temp
+        max_idx = np.argmax(A[j:, j].__abs__(), axis=0) 
+        if j != max_idx:
+            P[[j, max_idx]] = P[[max_idx, j]]
+            A[[max_idx, j]] = A[[j, max_idx]]
         for i in range(j+1, n):
             A[i, j] /= A[j, j]
             for k in range(j+1, n):
@@ -25,13 +23,18 @@ def gemm(A: np.ndarray) -> np.ndarray:
 def main():
     n = 3
     A = np.random.random((n, n))
+    A_cp = A.copy()
     print(f"matrix A:\n {A}")
 
-    P = gemm(A)
+    Perm = gemm(A)
     print(f"matrix A:\n {A}")
-    print(f"array P: {P}")
+    print(f"array P: {Perm}")
 
-    # Bsp, Psp = la.lu()
+    P, l, U = la.lu(A_cp)
+    print(f"matrix L:\n {l}")
+    print(f"array U: {U}")
+
+    print(f"perm A:\n{A[Perm]}")
 
 if __name__ == "__main__":
     main()
